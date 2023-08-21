b187191f2aa77d36947d4386112aa8fe5262012d
Anuj Patel <anuj4860@gmail.com>
2023-08-21 14:39:22 -0600

    Added liked sounds controller func

---
+++ SleepWellApp/Client/Shared/AudioCard.razor.cs
@@ -0,0 +1,38 @@
+﻿using SleepWellApp.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+
+namespace SleepWellApp.Client.Shared;
+
+public partial class AudioCard
+{
+    [Parameter]
+    public int audioID { get; set; } // ID
+    [Parameter]
+    public string? audioTitle { get; set; } // Title
+    [Parameter]
+    public string? audioImage { get; set; } // Image
+    [Parameter]
+    public string? audioDesc { get; set; } // Description
+    [Parameter]
+    public string? audioLink { get; set; } // Direct link to audio file
+
+    public bool Liked { get; set; }
+
+    private bool isVisible;
+
+    public void OpenOverlay()
+    {
+        isVisible = true;
+        StateHasChanged();
+    }
+
+    public void ButtonOnClick(bool liked_toggle)
+    {
+        Liked = liked_toggle;
+        audioDesc = audioID.ToString();
+
+        //await Http.PostAsync($"api/audio/{audioID} / like", null);
+    }
+}
