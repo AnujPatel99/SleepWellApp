1cfff0a1d1d08587998011158ffdb8d8696ec30d
Anuj Patel <anuj4860@gmail.com>
2023-08-24 14:12:29 -0600

    added view for liked audio

--- SleepWellApp/Client/Pages/LikedAudio.razor.cs
+++ SleepWellApp/Client/Pages/LikedAudio.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Net.Http.Json;
 
 namespace SleepWellApp.Client.Pages
@@ -7,15 +8,18 @@ namespace SleepWellApp.Client.Pages
     {
         [Inject]
         public HttpClient _httpClient { get; set; } = new HttpClient();
+        [Inject]
+        public AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
         private List<int> audioIds = new List<int>();
-
-        public LikedAudio()
-        { // parameterless constructor because HTML was being mean - Anuj
-        }
 
         protected override async Task OnInitializedAsync()
         {
-            audioIds = await GetAudioIdsFromApi();
+            var UserAuth = (await AuthenticationStateProvider.GetAuthenticationStateAsync()).User.Identity;
+            if (UserAuth is not null && UserAuth.IsAuthenticated)
+            {
+                audioIds = await GetAudioIdsFromApi();
+            }
+            //audioIds = await GetAudioIdsFromApi();
         }
 
         public async Task<List<int>> GetAudioIdsFromApi()
