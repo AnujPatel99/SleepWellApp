fb4eebcdce3144284f7a863a041179448490c59a
Varad Mahajan <964877@cognizant.com>
2023-08-25 15:55:12 -0600

    minor UI adjustments

---
+++ SleepWellApp/Client/Pages/ViewJournals.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace SleepWellApp.Client.Pages
+{
+    public partial class ViewJournals
+    {
+
+        [Inject]
+        public HttpClient Http { get; set; } = new HttpClient();
+        [Inject]
+        public AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
+
+
+        //CODE TO VIEW JOURNALS GOES HERE
+    }
+}
